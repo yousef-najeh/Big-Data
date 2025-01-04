9636a2982c339dd1fb069e029b83c2383852da2a
Rajab M <rajab1.masri@gmail.com>
2025-01-04 22:58:21 +0200

    code improvemnt
    Update README.md
    
    added trend component

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,15 +1,17 @@
 import Chart from '../components/Chart';
-// import Map from '../components/Map';
-import {  useState } from 'react';
+import Map from '../components/Map';
+import { useState } from 'react';
 import { useQuery } from "@tanstack/react-query";
 import * as chartHelper from "../helpers/chartHelper";
 // import useGeoData from '../helpers/mapApiHook';
+import NavBar from '../components/NavBar';
+import Trend from '../components/Trend';
 
 
 const Search = () => {
     const [search, setSearch] = useState("");
-    const [start,setStart]=useState("");
-    const [end,setEnd]=useState("");
+    const [start, setStart] = useState("");
+    const [end, setEnd] = useState("");
     const [query, setQuery] = useState(false);
     // const [country,setCountry]=useState("")
     // const {location,loading,error}=useGeoData(country)
@@ -27,18 +29,11 @@ const Search = () => {
     // const coordinates_data=JSON.stringify(coordinates)
     // console.log(coordinates?.lat,coordinates?.lon)
 
-    console.log("search",search)
-    console.log("start",start)
-    console.log("end",end)
-    // console.log("country",country)
-
     const { data, isLoading, isError } = useQuery({
         queryKey: ["tweets", { query }],
         queryFn: async () => {
-            console.log("query", query);
-
             const response = await fetch(
-                    `http://127.0.0.1:3000/api/tweets/search?keyword=${search.trim()}&startTime=${start}&endTime=${end}`
+                `http://127.0.0.1:3000/api/tweets/search?keyword=${search.trim()}&startTime=${start}&endTime=${end}`
             );
             if (!response.ok) {
                 throw new Error("Network response was not ok");
@@ -48,29 +43,32 @@ const Search = () => {
         enabled: query,
         refetchInterval: 10000,
     });
-
+    const { data: trendTag, isLoading: isTrendLoading } = useQuery({
+        queryKey: ["trend"],
+        queryFn: async () => {
+            const response = await fetch("http://127.0.0.1:3000/api/tweets/trend");
+            return response.json();
+        },
+        refetchInterval: 60000,
+    });
     const chartData = data
         ? chartHelper.prepareChartData(data.chart)
         : [];
-
-        console.log(data)
 
     const handleSearchSubmit = (e) => {
         e.preventDefault();
         if (search.trim() !== "" || start !== "" || end !== "") {
             setQuery(true);
         }
-
-        
     };
 
-
-    if(isLoading){
+    if (isLoading || isTrendLoading) {
         return <div>Loading...</div>
     }
-    
+
     return (
         <div>
+            <NavBar />
             <form onSubmit={handleSearchSubmit}>
                 <input
                     type="text"
@@ -78,14 +76,14 @@ const Search = () => {
                     onChange={(e) => setSearch(e.target.value)}
                     placeholder="Search"
                 />
-                <input type="date" 
-                        value ={start}
-                        placeholder="start date"
-                        onChange={(e)=>setStart(e.target.value)}/>
                 <input type="date"
-                        value={end}
-                        placeholder="end date"
-                        onChange={(e)=>setEnd(e.target.value)}/>
+                    value={start}
+                    placeholder="start date"
+                    onChange={(e) => setStart(e.target.value)} />
+                <input type="date"
+                    value={end}
+                    placeholder="end date"
+                    onChange={(e) => setEnd(e.target.value)} />
                 {/* <input type="text"
                         value={country}
                         onChange={(e) => setCountry(e.target.value)}
@@ -101,7 +99,10 @@ const Search = () => {
                     <Chart chartData={chartData} />
                     <br />
                     <br />
-                    {/* <Map tweets={data.hits.hits} /> */}
+                    <Map tweets={data.map.hits.hits} />
+                    <div >
+                        <Trend trendTag={trendTag} />
+                    </div>
                 </div>
             )}
         </div>
